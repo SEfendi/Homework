fa9b184571ce4a6c328d01674d4974b0b027f61d
SEfendi <sarkhan.efendi@gmail.com>
2023-04-16 16:35:15 +0400

    Added 15.04.2023 Homework

---
+++ Homework1504/Homework1504/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Adiniz nedir");
+        string Name = Console.ReadLine();
+        Console.WriteLine("Soyadiniz nedir");
+        string Surname = Console.ReadLine();
+        Console.WriteLine("Nece yasiniz var ?");
+        string Age = Console.ReadLine();
+        Console.WriteLine(Name + " " + Surname + " " + Age);
+
+    }
+}
